74a7b8c4a9424975d14b2f864da85803a2a5df73
Ben Fairfield <benfa@umich.edu>
2018-01-18 13:40:34 -0500

    replace metrics with its-metrics in start.sh

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ ln -sf /secrets/app/settings.php /var/www/html/sites/default/settings.php
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
-ln -sf /secrets/ssl/metrics.openshift.dsc.umich.edu.cert /etc/ssl/certs/metrics.openshift.dsc.umich.edu.cert
-ln -sf /secrets/ssl/metrics.openshift.dsc.umich.edu.key /etc/ssl/private/metrics.openshift.dsc.umich.edu.key
+ln -sf /secrets/ssl/its-metrics.openshift.dsc.umich.edu.cert /etc/ssl/certs/its-metrics.openshift.dsc.umich.edu.cert
+ln -sf /secrets/ssl/its-metrics.openshift.dsc.umich.edu.key /etc/ssl/private/its-metrics.openshift.dsc.umich.edu.key
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs
